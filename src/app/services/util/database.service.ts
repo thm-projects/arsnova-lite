e482b3a4e516146821de8c47c4919b46987921ad
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-08-09 10:49:23 +0200

    Implement databaseService for comments using dexie

---
+++ src/app/services/util/database.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Dexie } from 'dexie';
+import { Comment } from '../../models/comment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService extends Dexie {
+
+  comments: Dexie.Table<Comment, string>;
+
+  constructor() {
+    super('indexedDB');
+    this.version(1).stores({
+      comments: 'id, roomId, userId, revision, body, read, correct, favorite, timestamp,' +
+        'createdFromLecturer, highlighted, ack'
+    });
+  }
+}
